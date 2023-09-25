ea955c7702eb547ca16d5168b07244c60b071ef7
Ameenah Burhan <ameenah.burhan@gmail.com>
2023-09-25 12:03:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,108 @@
+import argparse
+import os
+import pprint
+import time
+import uuid
+
+from google.api import metric_pb2  # type: ignore
+from google.api import monitored_resource_pb2  # type: ignore
+from google.cloud.monitoring_v3.services.metric_service import pagers
+
+async def write_to_bigquery(write_client, rows):       
+    parent = write_client.table_path(config.PROJECT_ID, config.BIGQUERY_DATASET, config.BIGQUERY_TABLE)
+    write_stream = types.WriteStream()
+    write_stream.type_ = types.WriteStream.Type.PENDING
+    write_stream = write_client.create_write_stream(
+        parent=parent, write_stream=write_stream
+    )
+    stream_name = write_stream.name
+
+    # Create a template with fields needed for the first request.
+    request_template = types.AppendRowsRequest()
+
+    # The initial request must contain the stream name.
+    request_template.write_stream = stream_name
+
+    # So that BigQuery knows how to parse the serialized_rows, generate a
+    # protocol buffer representation of your message descriptor.
+    proto_schema = types.ProtoSchema()
+    proto_descriptor = descriptor_pb2.DescriptorProto()
+    bigquery_schema_pb2.Record.DESCRIPTOR.CopyToProto(proto_descriptor)
+    proto_schema.proto_descriptor = proto_descriptor
+    proto_data = types.AppendRowsRequest.ProtoData()
+    proto_data.writer_schema = proto_schema
+    request_template.proto_rows = proto_data
+    
+    # Some stream types support an unbounded number of requests. Construct an
+    # AppendRowsStream to send an arbitrary number of requests to a stream.
+    append_rows_stream = writer.AppendRowsStream(write_client, request_template)
+
+    # Create a batch of row data by appending proto2 serialized bytes to the
+    # serialized_rows repeated field.
+    proto_rows = types.ProtoRows()
+    for row in rows:
+        proto_rows.serialized_rows.append(row)
+    request = types.AppendRowsRequest()
+    request.offset = 0
+    proto_data = types.AppendRowsRequest.ProtoData()
+    proto_data.rows = proto_rows
+    request.proto_rows = proto_data
+
+    append_rows_stream.send(request)
+
+    # Shutdown background threads and close the streaming connection.
+    append_rows_stream.close()
+
+    # A PENDING type stream must be "finalized" before being committed. No new
+    # records can be written to the stream after this method has been called.
+    write_client.finalize_write_stream(name=write_stream.name)
+
+    # Commit the stream you created earlier.
+    batch_commit_write_streams_request = types.BatchCommitWriteStreamsRequest()
+    batch_commit_write_streams_request.parent = parent
+    batch_commit_write_streams_request.write_streams = [write_stream.name]
+    write_client.batch_commit_write_streams(batch_commit_write_streams_request)
+
+    logging.info(f"Writes to stream: '{write_stream.name}' have been committed.")
+  
+def list_time_series(project_id: str) -> pagers.ListTimeSeriesPager:
+    """Prints CPU utilization metric collected during last 20 minutes
+
+    Args:
+        project_id: Google Cloud project id
+
+    Returns:
+        Collection of time series.
+        Iterating over this object will yield results and resolve additional pages automatically.
+    """
+    # [START monitoring_read_timeseries_simple]
+    from google.cloud import monitoring_v3
+
+    client = monitoring_v3.MetricServiceClient()
+    project_name = f"projects/{project_id}"
+    now = time.time()
+    seconds = int(now)
+    nanos = int((now - seconds) * 10**9)
+    interval = monitoring_v3.TimeInterval(
+        {
+            "end_time": {"seconds": seconds, "nanos": nanos},
+            "start_time": {"seconds": (seconds - 300), "nanos": nanos},
+        }
+    )
+
+    results = client.list_time_series(
+        request={
+            "name": project_name,
+            "filter": 'metric.type = "networking.googleapis.com/node_flow/egress_bytes_count"',
+            "interval": interval,
+            "view": monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL,
+        }
+    )
+    for result in results:
+        print(result)
+        quit()
+    # [END monitoring_read_timeseries_simple]
+    return results
+if __name__ == "__main__":
+    list_time_series("gke-opt-test-3")
 
